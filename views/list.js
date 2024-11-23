f7e6c1f3f0813269b21c7cfe7d966a3ae948c81f
IvanSanczewski <isanczewski@gmail.com>
2024-11-23 15:16:46 +0200

    Added list.cart.map() to iterate through the cart array in each list object

--- views/list.js
+++ views/list.js
@@ -6,9 +6,15 @@ const displayList = (list) => /*html*/`
 <li data-id="${list.id}"> //?? data-id what type of attribute is it??
     <div class="list-card">
         <h3>${list.title}</h3>
-        <p>${list.cart[0].item}</p>
-        <p>${list.cart[0].bought}</p>
         <p>${list.total}</p>
+        <ul>
+            ${list.cart.map((e) =>`
+                <li>
+                    <p>${e.bought ? 'YES' : 'NO'}</p> // substitute YES/NO by an icon or class defining colour
+                    <p>${e.item} ${e.units ? `X${e.units}` : ''}</p>
+                </li>`
+                ).join('')}
+        </ul>
         <p>${list.weekday}</p>
         <p>${list.shop}</p>
         <form action="insertItem">
