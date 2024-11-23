82b9a2c3346f69de26db0ef78f97427382928740
IvanSanczewski <isanczewski@gmail.com>
2024-11-23 14:15:13 +0200

    Updated list.js

--- views/list.js
+++ views/list.js
@@ -1,13 +1,13 @@
 // import SHOPPINGLISTS_DATA from "../data/data.js";
 // import displayCart from "./cart.js"
 
+
 const displayList = (list) => /*html*/`
 <li data-id="${list.id}"> //?? data-id what type of attribute is it??
     <div class="list-card">
         <h3>${list.title}</h3>
         <p>${list.cart[0].item}</p>
         <p>${list.cart[0].bought}</p>
-        
         <p>${list.total}</p>
         <p>${list.weekday}</p>
         <p>${list.shop}</p>
