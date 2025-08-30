f5e07d6a0d2560830492dba1ffe5fede5c9b268c
IvanSanczewski <isanczewski@gmail.com>
2025-08-31 02:08:01 +0300

    Fixed product edit & delete product btns position & edit quantity input legth.

--- views/cart.js
+++ views/cart.js
@@ -1,42 +1,46 @@
 const displayCart = (cart, listID, cartIndex) => /*html*/`
     <li class="cart-item ${cart.bought ? 'in-cart' : 'out-cart'}">
-        <span id="product-${listID}-${cartIndex}" class="item-product"
+        <div class="item-details">
+            <span id="product-${listID}-${cartIndex}" class="item-product"
             hx-post="/toggle-item-status"
             hx-vals='{"listID": "${listID}", "cartIndex": ${cartIndex}}'
             hx-target="closest li"
             hx-swap="outerHTML"
             style="cursor: pointer;"
-        >${cart.item}</span>
-
-        ${cart.units ?
-            `<span class="item-units"   
+            >${cart.item}</span>
+            
+            ${cart.units ?
+                `<span class="item-units"   
                 hx-get="/edit-quantity/${listID}/${cartIndex}/${cart.units}"
                 hx-target="this"
                 hx-swap="outerHTML"
                 style="cursor: pointer;"
                 title="Edit quantity"
-            >${cart.units}</span>` 
-        : 
-            `<span class="item-units"
+                >${cart.units}</span>` 
+                : 
+                `<span class="item-units"
                 hx-get="/edit-quantity/${listID}/${cartIndex}/${cart.units}"
                 hx-target="this"
                 hx-swap="outerHTML"
                 style="cursor: pointer;"
                 title="Add quantity"
-            >Add</span>`
-        }
-        
-        <button class="edit-product-btn"
-            hx-get="/edit-product/${listID}/${cartIndex}"
-            hx-target="#product-${listID}-${cartIndex}"
-            hx-swap="outerHTML"
-        >Edit Product</button>
+                >Add</span>`
+            }
+        </div>
 
-        <button class="delete-item"
-            hx-delete="/delete-product/${listID}/${cartIndex}"
-            hx-target="closest li"
-            hx-swap="outerHTML"
-        >Delete Product</button>
+        <div class="item-buttons">
+            <button class="edit-product-btn"
+                hx-get="/edit-product/${listID}/${cartIndex}"
+                hx-target="#product-${listID}-${cartIndex}"
+                hx-swap="outerHTML"
+            >Edit Product</button>
+
+            <button class="delete-item"
+                hx-delete="/delete-product/${listID}/${cartIndex}"
+                hx-target="closest li"
+                hx-swap="outerHTML"
+            >Delete Product</button>
+        </div>
     </li>
 `
 
